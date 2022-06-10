44c04ef55480ad6e93beb3d3483f9a589e77a170
RawanHamada <rawanahamada@gmail.com>
2022-06-10 18:31:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
 function App() {
+  const [state, usestate] =React.useState([]);
+
+  
+  React.useEffect(()=>{
+    const fetchApi= async()=>{
+        await fetch('https://629e71fe3dda090f3c19d701.mockapi.io/v1/meals')
+       .then((response) => response.json())
+       .then((data) => {usestate(data)});
+     }
+     fetchApi();
+   },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+    {state.map((element)=>(
+          <div className='card'>
+              <img src={element.image}/>
+              <p>{element.name}</p>
+          </div>
+    ))}
+  </div>
+  
+);
 }
 
 export default App;
